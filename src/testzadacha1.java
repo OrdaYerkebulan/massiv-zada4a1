6bda017b385ab28c4f0ee0f5993ccfc7bc0dec45
Orda Yerkebulan <ordaerkebulan@gmail.com>
2023-02-05 10:46:24 +0600

    massiv zadachafirst

---
+++ src/testzadacha1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class testzadacha1 {
+    public static void main(String[] args) {
+        int[] nums = {7, -3, 9, -11, 18, 99, 2, 11};
+        System.out.println("Исходный массив:  " + Arrays.toString(nums));
+        System.out.print("Первые 3 элемента:");
+        int count = 3;
+        for (int i = 0; i < count; i++) {
+            System.out.print(" " + nums[i]);
+        }
+    }
+}
